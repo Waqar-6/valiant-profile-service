bec03f0d4466d59b71572b84ee2cb7b72c83b537
Waqar <waqar-1@hotmail.co.uk>
2025-03-24 22:49:30 +0000

    documentation: add openapi documentation for profile controller

--- src/main/java/com/wfarooq/profile_service/ProfileServiceApplication.java
+++ src/main/java/com/wfarooq/profile_service/ProfileServiceApplication.java
@@ -1,9 +1,27 @@
 package com.wfarooq.profile_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "User Profile microservice Documentation",
+				description = "Valiant Paws Microservice Rest Api",
+				version = "V1",
+				contact = @Contact(
+						name = "Waqar Farooq",
+						email = "farooqwaqar18@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		)
+)
 public class ProfileServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/wfarooq/profile_service/controller/ProfileController.java
+++ src/main/java/com/wfarooq/profile_service/controller/ProfileController.java
@@ -3,11 +3,14 @@ package com.wfarooq.profile_service.controller;
 import com.wfarooq.profile_service.constants.ProfileConstants;
 import com.wfarooq.profile_service.dto.requests.CreateNormalUserProfileRequest;
 import com.wfarooq.profile_service.dto.requests.CreateBreederProfileRequest;
-import com.wfarooq.profile_service.dto.response.BaseProfileResponseDto;
-import com.wfarooq.profile_service.dto.response.BreederProfileResponseDto;
-import com.wfarooq.profile_service.dto.response.NormalUserProfileResponseDto;
-import com.wfarooq.profile_service.dto.response.ResponseDto;
+import com.wfarooq.profile_service.dto.response.*;
 import com.wfarooq.profile_service.service.IProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +23,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(
+        name = "CRUD REST API for profile service",
+        description = "crud functionality for all post, get, put, delete"
+)
 @CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/api/v1/profiles", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -33,6 +40,23 @@ public class ProfileController {
         this.profileService = profileService;
     }
 
+    @Operation(
+            summary = "Create Normal User Profile",
+            description = "post operation to create a profile for the normal user of the app"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Http Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Http Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @PostMapping("/normalUserProfile")
     public ResponseEntity<ResponseDto> createNormalUserProfile(@Valid @RequestBody CreateNormalUserProfileRequest request) {
         log.info("create normal user profile request {}", request);
@@ -41,6 +65,24 @@ public class ProfileController {
         return new ResponseEntity<>(new ResponseDto(ProfileConstants.STATUS_201, ProfileConstants.MESSAGE_201), HttpStatus.CREATED);
     }
 
+
+    @Operation(
+            summary = "Create Dog Breeder User Profile",
+            description = "post operation to create a profile for the dog breeder"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Http Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Http Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @PostMapping("/breederProfile")
     public ResponseEntity<ResponseDto> createBreederProfile (@Valid @RequestBody CreateBreederProfileRequest request) {
         log.info("create breeder profile request {}", request);
@@ -57,6 +99,33 @@ public class ProfileController {
         return new ResponseEntity<>(allProfiles, HttpStatus.OK);
     }
 
+
+    @Operation(
+            summary = "Fetch Profile by id",
+            description = "REST API to fetch Profile based on id provided"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Http Status NOT FOUND",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+
+    )
     @GetMapping("/{profileId}")
     public ResponseEntity<BaseProfileResponseDto> fetchProfileById (@PathVariable UUID profileId) {
         log.info("Received request to fetch profile by ID: {}", profileId);
@@ -64,6 +133,28 @@ public class ProfileController {
         log.info("Successfully fetched profile: {}", profileId);
         return new ResponseEntity<>(profile, HttpStatus.OK);
     }
+
+
+
+    @Operation(
+            summary = "Fetch all dog breeder profiles",
+            description = "REST API to fetch all dog breeder profiles"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+
+    )
 
     @GetMapping("/breederProfiles")
     public ResponseEntity<List<BreederProfileResponseDto>> fetchAllBreederProfiles () {
@@ -73,6 +164,26 @@ public class ProfileController {
         return new ResponseEntity<>(profiles, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Fetch all normal user profiles",
+            description = "REST API to fetch all normal user profiles"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+
+    )
+
     @GetMapping("/normalUserProfiles")
     public ResponseEntity<List<NormalUserProfileResponseDto>> fetchAllNormalUserProfiles () {
         log.info("Received request to fetch all normal user profiles");
@@ -81,6 +192,37 @@ public class ProfileController {
         return new ResponseEntity<>(profiles, HttpStatus.OK);
     }
 
+
+    @Operation(
+            summary = "Update normal user profile ",
+            description = "REST API to update an existing profile based on id"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Http Status NOT FOUND",
+                    content = @Content(
+                    schema = @Schema(implementation = ErrorResponseDto.class)
+            )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+
+    )
     @PutMapping("/normalUserProfiles/{profileId}")
     public ResponseEntity<ResponseDto> updateNormalUserProfile (@Valid @RequestBody CreateNormalUserProfileRequest request, @PathVariable UUID profileId) {
         boolean isUpdated = profileService.updateNormalUserProfile(request,profileId);
@@ -88,12 +230,73 @@ public class ProfileController {
                 new ResponseEntity<>(new ResponseDto(ProfileConstants.STATUS_417, ProfileConstants.MESSAGE_417_UPDATE), HttpStatus.EXPECTATION_FAILED);
     }
 
+    @Operation(
+            summary = "Update dog breeder profile",
+            description = "REST API to update an existing profile based on id"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Http Status NOT FOUND",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+
+    )
     @PutMapping("/breederProfiles/{profileId}")
     public ResponseEntity<ResponseDto> updateBreederProfile (@Valid @RequestBody CreateBreederProfileRequest request, @PathVariable UUID profileId) {
         boolean isUpdated = profileService.updateBreederProfile(request, profileId);
         return isUpdated ? new ResponseEntity<>(new ResponseDto(ProfileConstants.STATUS_200, ProfileConstants.MESSAGE_200_UPDATE), HttpStatus.OK) :
                 new ResponseEntity<>(new ResponseDto(ProfileConstants.STATUS_417, ProfileConstants.MESSAGE_417_UPDATE), HttpStatus.EXPECTATION_FAILED);
     }
+
+    @Operation(
+            summary = "Delete  profile",
+            description = "REST API to delete an existing profile based on id"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Http Status NOT FOUND",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+
+    )
 
     @DeleteMapping("/{profileId}")
     public ResponseEntity<ResponseDto> deleteProfileById (@PathVariable UUID profileId) {
